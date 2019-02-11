94200a708a407852d7a7581bde1eb6e70fb831da
Bohdan Korzhak <bohdan.korzhak@std.community>
2019-02-11 19:35:04 +0200

    Дані охолодження з 21*С по 7*С за 1.2 год.
    Температура кімнати на кінець експерименту - 19*С

--- thermostat/main.py
+++ thermostat/main.py
@@ -1,46 +1,56 @@
 import serial
 import matplotlib.pyplot as plt
-import numpy as np
+import csv
 from datetime import datetime
-import re
+
 
 temp_x = []
 temp_y = []
 hum_x = []
 hum_y = []
 
-
-HUMIDITY = 1
-TEMPERATURE = 2
-HEAT_INDEX = 3
-
 pattern = '^Humidity: ([0-9.]+) per.  Temperature: ([0-9.]+) C Heat index: ([0-9.]+) C$'
 
 ser = serial.Serial('COM6', 9600)
-i = 600
+i = 2400
 j = 0
+
+csv_header = [["time", "temp", "humidity"]]
+
+csvFile = open('serialReader/temp_data.csv', 'w')
+
+writer = csv.writer(csvFile)
+writer.writerows(csv_header)
+
 while i>0:
     data = ser.readline().decode()
     print(f"{data} | iter {i}")
+
+    current_time = datetime.now().time().isoformat()[:-7]
+    temperature = data[35:40]
+    humidity = data[10:15]
+
+    writer.writerows([[current_time, temperature, humidity]])
+
     if j == 0:
-        temp_y.append(data[35:40])
-        temp_x.append(datetime.now().time().isoformat()[:-7])
+        temp_x.append(current_time)
+        temp_y.append(temperature)
 
-        hum_y.append(data[10:15])
-        hum_x.append(datetime.now().time().isoformat()[:-7])
-
-    j = j + 1 if j<60 else 0
+    j = j + 1 if j<240 else 0
 
 
     i -= 1
 
+csvFile.close()
+
 fig = plt.figure()
 ax = plt.subplot(111)
 ax.plot(temp_x, temp_y, label='y = temperature\nx = time')
-plt.title('Залежність зміни температури від часу при охолодженні')
+plt.gca().invert_yaxis()
 plt.gcf().autofmt_xdate()
+plt.title('Залежність зміни температури від часу при охолодженні')
 ax.legend()
-fig.savefig('plot1.png')
+fig.savefig('serialReader/plot.png')
 
 plt.show()
 
